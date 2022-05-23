9a8f425b944fd6039ca941c2748f2ed4a5105223
nickjevershed <nick.evershed@gmail.com>
2022-05-23 17:32:56 +1000

    single index

--- overlap.py
+++ overlap.py
@@ -27,5 +27,24 @@ for electorate in electorates:
 		results_gp_t['electorate'] = electorate['properties']['CED_NAME21']
 		all_results.append(results_gp_t.to_dict('records')[0])
 
+#%%
 final = pd.DataFrame(all_results)
-final[['electorate','Major Cities of Australia','Inner Regional Australia','Outer Regional Australia','Remote Australia','Very Remote Australia',]].to_csv('electorate-remoteness.csv', index=False)
+
+indices = {'Major Cities of Australia':1, 'Inner Regional Australia':25, 'Outer Regional Australia':50,'Remote Australia':75,'Very Remote Australia':100}
+
+#%%
+def makeIndex(row):
+	index = 0
+	for key in indices:
+		if pd.notnull(row[key]):
+			result = row[key]/100 * indices[key]
+			index = index + result
+	return index		
+
+	
+final['remoteness_index'] = final.apply(makeIndex, axis=1)
+	
+#%%	
+
+final[['electorate','Major Cities of Australia','Inner Regional Australia','Outer Regional Australia','Remote Australia','Very Remote Australia','remoteness_index']].to_csv('electorate-remoteness.csv', index=False)
+
